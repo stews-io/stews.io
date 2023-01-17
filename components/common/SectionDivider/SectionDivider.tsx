0e8ec63c41b61f4278cc1860b103b88f105d24e2
jmath <clumsycomputer@gmail.com>
2023-01-17 14:24:35 -0700

    refactor(package.json): remove unused dependencies

--- components/common/SectionDivider/SectionDivider.tsx
+++
@@ -1,7 +0,0 @@
-import sectionDividerStyles from "./SectionDivider.module.scss";
-
-export function SectionDivider() {
-  return (
-    <div role={"separator"} className={sectionDividerStyles.sectionDivider} />
-  );
-}
