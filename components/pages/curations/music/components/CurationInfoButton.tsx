8a748b0d83f6cd5dd82b770e7b5719b71a60ee14
jmath <clumsycomputer@gmail.com>
2023-01-23 09:30:25 -0700

    feature(CurationInfoButton): progress on CurationInfoDisplay

--- components/pages/curations/music/components/CurationInfoButton.tsx
+++ components/pages/curations/music/components/CurationInfoButton.tsx
@@ -1,13 +1,31 @@
-import { Fragment } from "react";
+import Link from "next/link";
+import { Dispatch, Fragment, ReactNode, SetStateAction, useState } from "react";
+import ClickAwayListener from "react-click-away-listener";
+import { usePopper } from "react-popper";
 import styles from "./CurationInfoButton.module.scss";
 
 export interface CurationInfoButtonProps {}
 
 export function CurationInfoButton(props: CurationInfoButtonProps) {
   const {} = props;
+  const [infoButtonElement, setInfoButtonElement] =
+    useState<HTMLDivElement | null>(null);
+  const [infoDisplay, setInfoDisplay] = useState<ReactNode>(null);
   return (
     <Fragment>
-      <div tabIndex={0} className={styles.infoButton}>
+      <div
+        tabIndex={0}
+        className={styles.infoButton}
+        ref={setInfoButtonElement}
+        onClick={() => {
+          setInfoDisplay(
+            <CurationInfoDisplay
+              infoButtonElement={infoButtonElement}
+              setInfoDisplay={setInfoDisplay}
+            />
+          );
+        }}
+      >
         <svg viewBox="0 0 100 100" width={32} height={32}>
           <circle
             cx={50}
@@ -33,6 +51,84 @@ export function CurationInfoButton(props: CurationInfoButtonProps) {
           </text>
         </svg>
       </div>
+      {infoDisplay}
     </Fragment>
+  );
+}
+
+interface CurationInfoDisplayProps {
+  infoButtonElement: HTMLDivElement | null;
+  setInfoDisplay: Dispatch<SetStateAction<ReactNode>>;
+}
+
+function CurationInfoDisplay(props: CurationInfoDisplayProps) {
+  const { infoButtonElement, setInfoDisplay } = props;
+  const [infoDisplayElement, setInfoDisplayElement] =
+    useState<HTMLDivElement | null>(null);
+  const infoDisplayPopper = usePopper(infoButtonElement, infoDisplayElement, {
+    placement: "bottom-end",
+    modifiers: [
+      {
+        name: "offset",
+        options: {
+          offset: [3, -30],
+        },
+      },
+    ],
+  });
+  return (
+    <ClickAwayListener
+      onClickAway={() => {
+        setInfoDisplay(null);
+      }}
+    >
+      <div
+        className={styles.infoDisplayContainer}
+        {...infoDisplayPopper.attributes.popper}
+        style={{ ...infoDisplayPopper.styles.popper }}
+        ref={setInfoDisplayElement}
+      >
+        <div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              fontWeight: "bold",
+              fontSize: 20,
+            }}
+          >
+            music
+          </div>
+          <div
+            style={
+              {
+                // paddingTop: 1,
+              }
+            }
+          >
+            <Link legacyBehavior href={"./"}>
+              <a
+                style={{
+                  // fontStyle: "italic",
+                  fontSize: 16,
+                  fontWeight: "bold",
+                }}
+              >
+                clumsycomputer
+              </a>
+            </Link>
+          </div>
+        </div>
+        <div
+          style={{
+            paddingBlock: 8,
+            fontStyle: "italic",
+            fontSize: 14,
+          }}
+        >
+          some of my favorite music that i live my life too
+        </div>
+      </div>
+    </ClickAwayListener>
   );
 }
