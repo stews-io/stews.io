c889d50b64b0b92750bd5908aa30f003611ff56f
Jared Mathews <boat@sea.local>
2023-09-01 21:43:59 -0600

    feature(curations/bakedgoods): moar items

--- curations/bakedgoods/musicItems.ts
+++ curations/bakedgoods/musicItems.ts
@@ -122,4 +122,181 @@ export const musicItems: Array<MusicItem> = [
       }),
     ],
   },
+  {
+    itemId: 4,
+    musicType: 'source',
+    sourceType: 'collection',
+    collectionType: 'album',
+    musicThumbnailHref:
+      'https://m.media-amazon.com/images/I/51R1OMTsOeL._UX500_FMwebp_QL85_.jpg',
+    musicYear: 2020,
+    musicTitle: 'Neon Jungle',
+    musicArtist: ['CloZee'],
+    recordingContext: ['studio'],
+    musicTags: ['electronic'],
+    externalLinks: [
+      getSoundcloudLinkData({
+        soundcloudHref: 'https://soundcloud.com/clozee/sets/neon-jungle',
+      }),
+      getYoutubeLinkData({
+        youtubeHref:
+          'https://youtu.be/B0C7W5QDo1A?list=OLAK5uy_m6HmTn43kRNmTgBRIw5m2VvfKiUSqdC50&si=wa-37XDkwxgvIoNb',
+      }),
+      getSpotifyLinkData({
+        spotifyHref:
+          'https://open.spotify.com/album/63HO6iGZB9OZRpW9gMEIwg?autoplay=true',
+      }),
+      getAppleLinkData({
+        appleHref: 'https://music.apple.com/us/album/neon-jungle/1683581375',
+      }),
+    ],
+  },
+  {
+    itemId: 5,
+    musicType: 'source',
+    sourceType: 'collection',
+    collectionType: 'album',
+    musicThumbnailHref:
+      'https://m.media-amazon.com/images/I/41D2abak8LL._UX500_FMwebp_QL85_.jpg',
+    musicYear: 2022,
+    musicTitle: 'Actual Life 3',
+    musicArtist: ['Fred again..'],
+    recordingContext: ['studio'],
+    musicTags: ['electronic'],
+    externalLinks: [
+      getSoundcloudLinkData({
+        soundcloudHref:
+          'https://soundcloud.com/fredagain/sets/actual-life-3-january-1',
+      }),
+      getYoutubeLinkData({
+        youtubeHref:
+          'https://youtu.be/jRVnYzlyCh4?list=PLlAz0EOC2qClB2rtTkuxrzxdG-Y2GI4AG&si=08yMWXvhgp_vR8F7',
+      }),
+      getSpotifyLinkData({
+        spotifyHref:
+          'https://open.spotify.com/album/5q2iMctlDvEMYVIawF6Vop?autoplay=true',
+      }),
+      getAppleLinkData({
+        appleHref:
+          'https://music.apple.com/us/album/actual-life-3-january-1-september-9-2022/1640463893',
+      }),
+    ],
+  },
+  {
+    itemId: 6,
+    musicType: 'source',
+    sourceType: 'collection',
+    collectionType: 'album',
+    musicThumbnailHref:
+      'https://m.media-amazon.com/images/I/61cwTi50cVL._UX500_FMwebp_QL85_.jpg',
+    musicYear: 2018,
+    musicTitle: 'Amniotic',
+    musicArtist: ['Monolink'],
+    recordingContext: ['studio'],
+    musicTags: ['electronic'],
+    externalLinks: [
+      getSoundcloudLinkData({
+        soundcloudHref:
+          'https://soundcloud.com/monolink/sets/monolink-amniotic-album-2018',
+      }),
+      getYoutubeLinkData({
+        youtubeHref:
+          'https://youtu.be/q-z7bkisv7E?list=OLAK5uy_n2GQI4aC_0Ge_VzUToipYHOh9arqP2oho&si=-_krG3TnirX_b157',
+      }),
+      getSpotifyLinkData({
+        spotifyHref:
+          'https://open.spotify.com/album/3JdtES0NPLHDJZgCf3ZxUz?autoplay=true',
+      }),
+      getAppleLinkData({
+        appleHref:
+          'https://music.apple.com/us/album/amniotic-deluxe-version/1493969293',
+      }),
+    ],
+  },
+  {
+    itemId: 7,
+    musicType: 'source',
+    sourceType: 'collection',
+    collectionType: 'album',
+    musicThumbnailHref:
+      'https://m.media-amazon.com/images/I/41fZSy5x6BL._UX500_FMwebp_QL85_.jpg',
+    musicYear: 2012,
+    musicTitle: 'shrines',
+    musicArtist: ['Purity Ring'],
+    recordingContext: ['studio'],
+    musicTags: ['electronic'],
+    externalLinks: [
+      getYoutubeLinkData({
+        youtubeHref:
+          'https://youtu.be/O9DmOxGmE9E?list=OLAK5uy_mQIAkzEeYLoYyNIHIOT0vx2RoJBwyLe6M&si=FdMy3xaudvhqUkhI',
+      }),
+      getSpotifyLinkData({
+        spotifyHref:
+          'https://open.spotify.com/album/34qsY2V9hsXviGPlq5tvco?autoplay=true',
+      }),
+      getAppleLinkData({
+        appleHref: 'https://music.apple.com/us/album/shrines/535907905',
+      }),
+    ],
+  },
+  {
+    itemId: 8,
+    musicType: 'source',
+    sourceType: 'collection',
+    collectionType: 'album',
+    musicThumbnailHref:
+      'https://m.media-amazon.com/images/I/51SdfamIqXL._UX500_FMwebp_QL85_.jpg',
+    musicYear: 2018,
+    musicTitle: 'Kingdoms in Colour',
+    musicArtist: ['Maribou State'],
+    recordingContext: ['studio'],
+    musicTags: ['electronic'],
+    externalLinks: [
+      getSoundcloudLinkData({
+        soundcloudHref:
+          'https://soundcloud.com/mariboustate/sets/kingdoms-in-colour',
+      }),
+      getYoutubeLinkData({
+        youtubeHref:
+          'https://youtu.be/oyX3EWdtbmA?list=OLAK5uy_nzhKUS--ZCqL3glx8sDnHBtqhiSAsPAi8&si=skXaZGZoZU9ZtBlp',
+      }),
+      getSpotifyLinkData({
+        spotifyHref:
+          'https://open.spotify.com/album/70FGsJuLXPQHYdKmEZZFq9?autoplay=true',
+      }),
+      getAppleLinkData({
+        appleHref:
+          'https://music.apple.com/us/album/kingdoms-in-colour/1385072561',
+      }),
+    ],
+  },
+  {
+    itemId: 9,
+    musicType: 'source',
+    sourceType: 'collection',
+    collectionType: 'album',
+    musicThumbnailHref:
+      'https://m.media-amazon.com/images/I/51H9anLEewL._UX500_FMwebp_QL85_.jpg',
+    musicYear: 2021,
+    musicTitle: 'Isles',
+    musicArtist: ['BICEP'],
+    recordingContext: ['studio'],
+    musicTags: ['electronic'],
+    externalLinks: [
+      getSoundcloudLinkData({
+        soundcloudHref: 'https://soundcloud.com/feelmybicep/sets/bicep-isles',
+      }),
+      getYoutubeLinkData({
+        youtubeHref:
+          'https://youtu.be/oyX3EWdtbmA?list=OLAK5uy_nzhKUS--ZCqL3glx8sDnHBtqhiSAsPAi8&si=skXaZGZoZU9ZtBlp',
+      }),
+      getSpotifyLinkData({
+        spotifyHref:
+          'https://open.spotify.com/album/0EdtTRCl3J22AnWrNpH1w9?autoplay=true',
+      }),
+      getAppleLinkData({
+        appleHref: 'https://music.apple.com/us/album/isles/1531704818',
+      }),
+    ],
+  },
 ]
